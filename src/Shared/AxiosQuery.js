a39f0eb33384f79f19a893641ceac681606c64f5
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2020-12-01 00:13:15 +0200

    fixed btn goood

--- src/Shared/AxiosQuery.js
+++ src/Shared/AxiosQuery.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const FetchDataWithQuery=(query, page, APIkey )=>{
+const FetchDataWithQuery = (query, page, APIkey) => {
   return axios
-  .get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=${APIkey}&image_type=photo&orientation=horizontal&per_page=12`,
-  )
-  .then(response=>response.data.hits)
-}
-export default FetchDataWithQuery;
+    .get(
+      `https://pixabay.com/api/?q=${query}&page=${page}&key=${APIkey}&image_type=photo&orientation=horizontal&per_page=12`,
+    )
+    .then(response => response.data.hits);
+};
+export default { FetchDataWithQuery };
